97526695925331b0f236e6145a9ac990d357a2dd
Josef Zavisek <josef.zavisek@strv.com>
2017-05-24 19:29:01 +0200

    Add koa body middleware

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 'use strict'
 
 const Koa = require('koa')
+const Promise = require('bluebird')
+const koaBody = require('koa-body')
+const koaCompress = require('koa-compress')
 
-console.log(test)
-
-const app = new Koa()
+global.Promise = Promise
 
 const port = 3000
 const log = {
@@ -12,9 +13,30 @@ const log = {
   error: console.error,
 }
 
+const app = new Koa()
+app.use(koaCompress())
+app.use(koaBody({ multipart: true }))
+
+app.use(async (ctx, nextMiddleware) => {
+  log.info('Incoming request')
+  await nextMiddleware()
+})
+
+app.use(async ctx => {
+  log.info(ctx.request.headers)
+  log.info(ctx.request.body)
+  await Promise.delay(1000)
+  ctx.status = 200
+  ctx.body = {
+    status: 'runnning',
+  }
+})
+
 app.start = () => {
   log.info('Starting server ...')
   app.listen(port, () => {
     log.info(`==> 🌎 Server running on port ${port}.`)
   })
 }
+
+app.start()
